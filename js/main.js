c0bd9bcac69e2728236fd34d33fa1cd50e1f5a42
suronjit797 <suronjit797@gmail.com>
2022-02-17 03:44:43 +0600

    js added with function

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ function inputValue(id) {
 }
 
 
-// calculate button click
-selectorId('calculate').addEventListener('click', function () {
+// income expanse function 
+function inputOutputBalance() {
     // income
     let income = inputValue('income')
 
@@ -18,8 +18,9 @@ selectorId('calculate').addEventListener('click', function () {
     let food = inputValue('food')
     let rent = inputValue('rent')
     let clothes = inputValue('clothes')
-
-    // error handling
+    let totalExpanse = food + rent + clothes
+    let balance = 0
+    // error handling of income and expanse
     if (isNaN(income) || isNaN(food) || isNaN(rent) || isNaN(clothes)) {
         selectorId('modal_message').innerText = 'Please fill all input boxes with number'
         selectorId('modal_close').style.display = 'block'
@@ -27,19 +28,46 @@ selectorId('calculate').addEventListener('click', function () {
         selectorId('modal_message').innerText = 'Please fill all number positive'
         selectorId('modal_close').style.display = 'block'
     }
-    else if(income > totalExpanse){
-        let totalExpanse = food + rent + clothes
+    else if (income < totalExpanse) {
+        selectorId('modal_message').innerText = 'Your expanse is mode than your income'
+        selectorId('modal_close').style.display = 'block'
+    } else {
         selectorId('totalExpense').innerText = totalExpanse
+        balance = income - totalExpanse
+        selectorId('balanceAmount').innerText = balance
     }
+    return balance
+}
+
+// calculate button click
+selectorId('calculate').addEventListener('click', inputOutputBalance)
 
 
+selectorId('save_btn').addEventListener('click', function () {
+    let percentageTextValue = selectorId('percentage')
+    let percentage = parseFloat(percentageTextValue.value)
+    let previousBalance = inputOutputBalance()
+
+    let savingAmount = 0
 
 
-    // console.log(totalExpanse)
-
+    if (previousBalance <= 0) {
+        selectorId('modal_message').innerText = 'You have not any balance'
+        selectorId('modal_close').style.display = 'block'
+    } else if(!percentage || isNaN(percentage)){
+        selectorId('modal_message').innerText = 'Please provide a valid percentage value'
+        selectorId('modal_close').style.display = 'block'
+    } else if (percentage > 100) {
+        selectorId('modal_message').innerText = 'You cannot save more than you have'
+        selectorId('modal_close').style.display = 'block'
+    }
+    
+    else {
+        savingAmount = (previousBalance * percentage) / 100
+        selectorId('savingAmount').innerText = savingAmount
+        selectorId('remainingBalance').innerText = previousBalance - savingAmount
+    }
 })
-
-
 
 
 
